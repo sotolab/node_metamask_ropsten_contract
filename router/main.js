b07819e5131a64a5490017be3d940d26ad843e59
sotojang <mrjang28@gmail.com>
2022-04-29 11:02:32 +0900

    Update main.js

--- router/main.js
+++ router/main.js
@@ -2,7 +2,7 @@
 const Tx = require('ethereumjs-tx').Transaction
 const Web3 = require('web3')
 const abi = require('../public/js/contractabi.json')
-const web3 = new Web3('https://ropsten.infura.io/v3/3c52917848e945229c0d33d632b10490')
+const web3 = new Web3('https://ropsten.infura.io/v3/')
 
 const account = ''
 
